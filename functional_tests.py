d912de708411ecb545cdf7bd96333a464e290505
Yousef Hamza <jo.adam.93@gmail.com>
2015-10-28 22:22:57 +0200

    Extending our functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest (unittest.TestCase):
@@ -14,17 +15,31 @@ class NewVisitorTest (unittest.TestCase):
 
 		#User notice title is 'to-do'
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
 		#User invited to add a to-do item
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
 
 		#User types "Buy peacock feather" into a textbox
+		inputbox.send_keys('Buy peacock feather')
 
 		#When user press enter	the page updates showing
 		# "1:Buy peacock feather" as item in to-do list
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feather' for row in rows)
+		)
 
 		#There's stil a box to enter another item
-
+		self.fail('Finish the test!')
 		#User enter "Use peacock feather to make a fly"
 
 		#The page updates again showing both items
