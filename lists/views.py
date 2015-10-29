e75a544fd9f796b727b64e6cf855418128efa290
Yousef Hamza <jo.adam.93@gmail.com>
2015-10-29 13:44:01 +0200

    Refactoring post

--- lists/views.py
+++ lists/views.py
@@ -5,16 +5,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_page(request):
-	if request.method == 'POST':
-		new_item_text = request.POST['item_text']
-		Item.objects.create(text = new_item_text)
-		return redirect('/lists/the-only-list-in-the-world/')
-	else:
-		items = Item.objects.all()
-		return render(
-			request,
-			'home.html'
-		)
+	return render(
+		request,
+		'home.html'
+	)
 def view_list(request):
 	items = Item.objects.all()
 	return render(
@@ -24,3 +18,6 @@ def view_list(request):
 			'items': items,
 		}
 	)
+def new_list(request):
+	Item.objects.create(text=request.POST['item_text'])
+	return redirect('/lists/the-only-list-in-the-world/')
