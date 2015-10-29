f9e8a39111001ad27da40cbb3e575c69f690a878
Yousef Hamza <jo.adam.93@gmail.com>
2015-10-29 21:08:22 +0200

    moving sqlite db and extending functional test

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,8 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import sys
 
 class NewVisitorTest (StaticLiveServerTestCase):
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+		super(NewVisitorTest, NewVisitorTest).setUpClass()
+		cls.server_url = cls.live_server_url
+
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super(NewVisitorTest, NewVisitorTest).tearDownClass()
+
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 
@@ -16,7 +31,7 @@ class NewVisitorTest (StaticLiveServerTestCase):
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		#User to website homepage
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 
 		#User notice title is 'to-do'
 		self.assertIn('To-Do', self.browser.title)
@@ -64,7 +79,7 @@ class NewVisitorTest (StaticLiveServerTestCase):
 		self.browser = webdriver.Firefox()
 
 		#New user visit the home page. There is no sign of first user list
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
@@ -80,7 +95,7 @@ class NewVisitorTest (StaticLiveServerTestCase):
 		self.assertNotEqual(new_user_url, user_list_url)
 
 	def test_layout_and_styling(self):
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024, 768)
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
