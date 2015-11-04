810b73cc4edbae61a1a41da118919f052c5cc447
Yousef Hamza <jo.adam.93@gmail.com>
2015-11-04 15:11:20 +0200

    Refactor list view to handle new item POSTs

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -42,31 +42,6 @@ class ListViewTest(TestCase):
 
 		self.assertTemplateUsed(response, 'list.html')
 
-#	def test_uses_list_template(self):
-#		response = self.client.get('/lists/the-only-list-in-the-world/')
-#		self.assertTemplateUsed(response, 'list.html')
-
-	def test_saving_a_POST_request(self):
-		self.client.post(
-			'/lists/new',
-			data= {
-				'item_text': 'A new list item'
-			}
-		)
-		self.assertEqual(Item.objects.count(), 1)
-		new_item = Item.objects.first()
-		self.assertEqual(new_item.text, 'A new list item')
-
-	def test_redirects_after_POST(self):
-		response = self.client.post(
-			'/lists/new',
-			data= {
-				'item_text': 'A new list item'
-			}
-		)
-		new_list = List.objects.first()
-		self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
-
 	def test_passes_correct_list_to_tempalate(self):
 		other_list = List.objects.create()
 		correct_list = List.objects.create()
@@ -74,13 +49,12 @@ class ListViewTest(TestCase):
 		response = self.client.get('/lists/%d/' % (correct_list.id,))
 		self.assertEqual(response.context['list'], correct_list)
 
-class NewItemTest(TestCase):
 	def test_can_save_a_POST_request_to_an_existing_list(self):
 		other_list = List.objects.create()
 		correct_list = List.objects.create()
 
 		self.client.post(
-			'/lists/%d/add_item' % (correct_list.id,),
+			'/lists/%d/' % (correct_list.id,),
 			data = {
 				'item_text': 'A new item for an existing list'
 			}
@@ -96,31 +70,9 @@ class NewItemTest(TestCase):
 		correct_list = List.objects.create()
 
 		response = self.client.post(
-			'/lists/%d/add_item' % (correct_list.id,),
+			'/lists/%d/' % (correct_list.id,),
 			data = {
 				'item_text': 'A new item for an exisitng list'
 			}
 		)
 		self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
-
-	def test_validation_errors_are_sent_back_to_home_page_template(self):
-		response = self.client.post(
-			'/lists/new',
-			data= {
-				'item_text': '',
-			}
-		)
-		self.assertEqual(response.status_code, 200)
-		self.assertTemplateUsed(response, 'home.html')
-		expected_error = escape('You can\'t have an empty list item')
-		self.assertContains(response, expected_error)
-
-	def test_invalid_list_items_arenot_saved(self):
-		self.client.post (
-			'/lists/new',
-			data = {
-				'item_text': ''
-			}
-		)
-		self.assertEqual(List.objects.count(), 0)
-		self.assertEqual(Item.objects.count(), 0)
--- lists/views.py
+++ lists/views.py
@@ -12,6 +12,9 @@ def home_page(request):
 	)
 def view_list(request, list_id):
 	list_ = List.objects.get(id=list_id)
+	if request.method == 'POST':
+		Item.objects.create(text=request.POST['item_text'], list=list_)
+		return redirect('/lists/%d/' % (list_.id,))
 	items = Item.objects.filter(list=list_)
 	return render(
 		request,
@@ -38,9 +41,4 @@ def new_list(request):
 					'error': error
 				}
 			)
-	return redirect('/lists/%d/' % (list_.id,))
-
-def add_item(request, list_id):
-	list_ = List.objects.get(id=list_id)
-	Item.objects.create(text=request.POST['item_text'], list=list_)
 	return redirect('/lists/%d/' % (list_.id,))
